903cd255b7d3828064363a7d5227c7f8f40af901
Eshin Griffith <eshin@me.com>
2022-03-31 09:20:49 -0400

    Iterate over all repos in org

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { getAllPullRequests, getRepoNames } from "./modules/svc.github"
 import { PullRequest } from "./modules/svc.github/types"
+const ORG = "ramda";
 
 (async () => {
-  // const ramdaRepos = await getRepoNames("Ramda")
   const ramdaPulls: PullRequest[] = []
-  for await (const prChunk of getAllPullRequests("ramda", "ramda")) {
-    ramdaPulls.push(...prChunk)
+  for(const repo of await getRepoNames(ORG)) {
+    console.log(`Gathering PRs for ${repo}`)
+    for await (const prChunk of getAllPullRequests(ORG, repo)) {
+      ramdaPulls.push(...prChunk)
+    }
   }
-  console.log(`Ramda has ${ramdaPulls.length} PRs!`)
+  console.log(`Ramda Org has ${ramdaPulls.length} PRs!`)
 })()
