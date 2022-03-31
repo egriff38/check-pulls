5b91abce81927d3975f77f825343a82748373ed2
Eshin Griffith <eshin@me.com>
2022-03-31 10:24:37 -0400

    Added aggregate PRs by week

--- src/main.ts
+++ src/main.ts
@@ -11,4 +11,21 @@ const ORG = "ramda";
     }
   }
   console.log(`Ramda Org has ${ramdaPulls.length} PRs!`)
+
+  const today = +new Date()
+  const ONE_WEEK = 1000 * 3600 * 24 * 7
+  const PRsByWeek = ramdaPulls.reduce((acc, pr) => {
+    const date = +new Date(pr.created_at)
+    const weeksPast = Math.floor((today - date) / ONE_WEEK)
+    acc[weeksPast] ??= 0
+    acc[weeksPast]++
+    return acc
+  }, [] as number[])
+  console.table(PRsByWeek.reduce((acc, count, index) => {
+    if(count) acc.push({
+      "Weeks Ago": index + 1,
+      "Number of PRs": count,
+    })
+    return acc
+  },[] as {"Weeks Ago": number, "Number of PRs": number}[]))
 })()
